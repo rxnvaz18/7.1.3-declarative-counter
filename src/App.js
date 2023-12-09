bffc795dc5bce84a97e382e80b989bdd181ed93c
unknown <roxanvaz31@gmail.com>
2023-12-09 11:56:20 -0800

    finished declarative counter

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React, { useState } from 'react'
 
 function App(){
     let [ counter, changeCounter ] = useState(1)
+    window.changeCounter = changeCounter
     return (
+      <div>
         <h1>{counter}</h1>
+          <h4>{counter}</h4>
+      </div>
     )
 }
 
